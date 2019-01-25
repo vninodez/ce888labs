f045fea7f39dfa405c382bc4e2ca5b0ec89c5ff2
vninodez <vninodez@essex.ac.uk>
2019-01-25 20:20:19 +0000

    final scripts

--- labs/lab 2/Exercise 2 Bootstrap/bootstrap.py
+++ labs/lab 2/Exercise 2 Bootstrap/bootstrap.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 
-
+## PART 1: Creating the boostrap function
 
 def boostrap(sample, sample_size, iterations,ciparam):
     sample_matrix=np.zeros((iterations,sample_size))
@@ -15,14 +15,32 @@ def boostrap(sample, sample_size, iterations,ciparam):
     lower=np.percentile(data_mean_mx,ciparam/2)
     upper=np.percentile(data_mean_mx,100-ciparam/2)
     data_mean=np.mean(data_mean_mx)
-    print(data_mean_mx)
     return data_mean, lower, upper
 
-sample=np.arange(10,1000,2)
-sample_size=100
-iterations=10
-boostrap(sample, sample_size, iterations)
+## PART 2 Applying the boostrap function on the vehicles data
 
+vehicles_df = pd.read_csv('./vehicles.csv')
+vehicles_df.columns=['Currentfleet','Newfleet']
+vehicles_df2=vehicles_df[vehicles_df.Newfleet.notnull()]
+current_fleet_data = vehicles_df.values.T[0]
+new_fleet_data = vehicles_df2.values.T[1]
+
+cf=boostrap(current_fleet_data, current_fleet_data.shape[0],1000,5)
+
+print(("Current Fleet Mean: %f")%(cf[0]))
+print(("Current Fleet Lower: %f")%(cf[1]))
+print(("Current Fleet Upper: %f")%(cf[2]))
+
+nf=boostrap(new_fleet_data, new_fleet_data.shape[0],1000,5)
+
+print(("New Fleet Mean: %f")%(nf[0]))
+print(("New Fleet Lower: %f")%(nf[1]))
+print(("New Fleet Upper: %f")%(nf[2]))
+
+## The New fleet has a higher MPG mean, the 95% CI for the mean of the new fleet 
+# is between 29.1 MPG and 31.7 MPG, while for the Current Fleet it is between 19.4 and 20.9.
+
+## Plots on salaries using boostrap iterations
 
 if __name__ == "__main__":
 	df = pd.read_csv('./salaries.csv')
@@ -30,7 +48,7 @@ if __name__ == "__main__":
 	data = df.values.T[1]
 	boots = []
 	for i in range(100, 100000, 1000):
-		boot = boostrap(data, data.shape[0], i)
+		boot = boostrap(data, data.shape[0], i, 5)
 		boots.append([i, boot[0], "mean"])
 		boots.append([i, boot[1], "lower"])
 		boots.append([i, boot[2], "upper"])
@@ -48,6 +66,7 @@ if __name__ == "__main__":
 	#print ("Mean: %f")%(np.mean(data))
 	#print ("Var: %f")%(np.var(data))
 	
+
 
 
 	

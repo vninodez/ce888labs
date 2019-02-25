cea148450189e2b1071a0f1435aedd5fce2364d3
vninodez <vninodez@essex.ac.uk>
2019-02-25 14:27:14 +0000

    lab7

--- labs/lab3/test_libraries.py
+++ labs/lab3/test_libraries.py
@@ -1,12 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 28 12:14:15 2019
-
-@author: vnino
-"""
-
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sbs
-import tensorflow as tf
-import sklearn as sk
+import urllib.request
